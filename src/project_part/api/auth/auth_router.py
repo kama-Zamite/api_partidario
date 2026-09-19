e8cccb97db01b0bebabe4002290fe68c89b43ebb
kama-Zamite <pedrozamite1@gmail.com>
2026-09-19 20:57:33 +0100

    add turnfile nao login e na confirmacao do 2FA

--- src/project_part/api/auth/auth_router.py
+++ src/project_part/api/auth/auth_router.py
@@ -113,7 +113,7 @@ async def login(
     session: Session,
     token: Access_token,
     # backgroundTasks: BackgroundTasks,
-    # _captcha: Claudflare_turnfile
+    _captcha: Claudflare_turnfile
     ):
     """Endpoint para autenticação de usuário."""
 
