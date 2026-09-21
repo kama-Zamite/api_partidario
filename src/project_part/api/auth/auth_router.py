e905e0c7f75b56e74e4bc3ad459745a562af4707
kama-Zamite <pedrozamite1@gmail.com>
2026-09-21 08:27:19 +0100

    remocao do turnfile para testar o 2fa em producao via api

--- src/project_part/api/auth/auth_router.py
+++ src/project_part/api/auth/auth_router.py
@@ -120,7 +120,7 @@ async def login(
     session: Session,
     token: Access_token,
     # backgroundTasks: BackgroundTasks,
-    _captcha: Claudflare_turnfile
+    # _captcha: Claudflare_turnfile
     ):
     """Endpoint para autenticação de usuário."""
 
@@ -320,7 +320,7 @@ async def verify_2fa(
     body: Login2FARequest,
     session: Session,
     # backgroundTasks: BackgroundTasks,
-    _captcha: Claudflare_turnfile,
+    # _captcha: Claudflare_turnfile,
 ):
     """
     Endpoint para verificação de autenticação de dois fatores (2FA).
