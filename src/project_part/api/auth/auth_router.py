41849bbfc8c38c19f510300d459419295c0d564d
kama-Zamite <pedrozamite1@gmail.com>
2026-09-21 19:44:55 +0100

    remocao do turn file tanto no endpooint de login quanto no endpoint de verificar o 2fa

--- src/project_part/api/auth/auth_router.py
+++ src/project_part/api/auth/auth_router.py
@@ -132,7 +132,7 @@ async def login(
     session: Session,
     token: Access_token,
     # backgroundTasks: BackgroundTasks,
-    # _captcha: Claudflare_turnfile
+    _captcha: Claudflare_turnfile
     ):
     """Endpoint para autenticação de usuário."""
 
@@ -327,7 +327,7 @@ async def verify_2fa(
     body: Login2FARequest,
     session: Session,
     # backgroundTasks: BackgroundTasks,
-    # _captcha: Claudflare_turnfile,
+    _captcha: Claudflare_turnfile,
 ):
     """
     Endpoint para verificação de autenticação de dois fatores (2FA).
